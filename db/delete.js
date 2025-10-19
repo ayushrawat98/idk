6cc805190161094e2860c45db415aea3b4e89978
ayushrawat98 <aayushrawat0@gmail.com>
2025-10-19 18:15:24 +0530

    delete update

--- db/delete.js
+++ db/delete.js
@@ -1,8 +1,8 @@
 import instance from "./db.js"
 
 instance.db.exec(`
-	delete from posts where id = 36
-	delete from posts where id = 37
-	delete from posts where id = 38
-	delete from posts where id = 39
+	delete from posts where id = 36;
+	delete from posts where id = 37;
+	delete from posts where id = 38;
+	delete from posts where id = 39;
 	`)

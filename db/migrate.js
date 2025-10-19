b6581b351096c50434ddc55af3c216104ecf88a0
ayushrawat98 <aayushrawat0@gmail.com>
2025-10-19 14:26:56 +0530

    db migrate file

---
+++ db/migrate.js
@@ -0,0 +1,7 @@
+
+import instance from "./db"
+
+instance.insertBoard.run('b','random bitching')
+instance.insertBoard.run('fit','yog and fitness')
+instance.insertBoard.run('fa','fashion vastra')
+instance.insertBoard.run('g','tech nerds')

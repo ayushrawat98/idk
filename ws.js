92ce5e9a9574974f7d822b27d5c031ca134c396f
ayushrawat98 <aayushrawat0@gmail.com>
2025-10-23 16:57:18 +0530

    Better online count

--- ws.js
+++ ws.js
@@ -3,8 +3,11 @@ import WebSocket from "ws"
 
 const wss = new WebSocketServer({port : 8080, path : "/ws"})
 
+let map = {}
+
 function broadcastUserCount() {
-	const count = wss.clients.size
+	// const count = wss.clients.size
+	const count = Object.keys(map).length
 	wss.clients.forEach(client => {
 		if(client.readyState === WebSocket.OPEN){
 			client.send(count)
@@ -12,10 +15,13 @@ function broadcastUserCount() {
 	})
 }
 
-wss.on('connection', (ws) =>{
+wss.on('connection', (ws, req) =>{
 	console.log("connected")
+	const ip = req.headers['x-real-ip'].split(',')[0].trim();
+	map[ip] = 1
 	broadcastUserCount()
 	ws.on('close', () => {
+		delete map[ip]
 		broadcastUserCount()
 		console.log("closed")
 	})

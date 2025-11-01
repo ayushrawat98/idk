14d3e162bf8ad4da3d800c058cb9234bec8cc6a0
ayushrawat98 <aayushrawat0@gmail.com>
2025-11-01 22:51:49 +0530

    Board name change

--- db/migrate.js
+++ db/migrate.js
@@ -6,5 +6,8 @@ import instance from "./db.js"
 // instance.insertBoard('fa','vastra maya')
 // instance.insertBoard('g','nerds assemble')
 
-instance.db.exec("update posts set board_id = 1 , updated_at = '2025-10-31T13:48:49.323Z' where board_id != 1;")
-instance.db.exec('delete from boards where id in (2,3,4);')
+// instance.db.exec("update posts set board_id = 1 , updated_at = '2025-10-31T13:48:49.323Z' where board_id != 1;")
+// instance.db.exec('delete from boards where id in (2,3,4);')
+
+instance.db.exec("update boards set name = 'bakchodi' where id = 1;")
+

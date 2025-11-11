d1b463d11cba5e6f8c90a0e2de9ea864a156ad3f
ayushrawat98 <aayushrawat0@gmail.com>
2025-11-11 20:20:27 +0530

    ui update

--- routes/board.route.js
+++ routes/board.route.js
@@ -28,15 +28,15 @@ route.get('/board/:boardName', async (req, res, next) => {
 	const boardsList = instance.getBoards()
 	const currentBoard = boardsList.filter(board => board.name == req.params.boardName)[0]
 	let threadsList = instance.getThreads(currentBoard.id)
-	threadsList.forEach(t => {
-		let temp = JSON.parse(t['latest_replies'])
-		if(temp.length == 0){
-			temp = null
-		}else{
-			temp.reverse()
-		}
-		t['latest_replies'] = temp
-	})
+	// threadsList.forEach(t => {
+	// 	let temp = JSON.parse(t['latest_replies'])
+	// 	if(temp.length == 0){
+	// 		temp = null
+	// 	}else{
+	// 		temp.reverse()
+	// 	}
+	// 	t['latest_replies'] = temp
+	// })
 	// threadsList.forEach(t => t['latest_replies'].reverse())
 	// console.log(threadsList)
 
